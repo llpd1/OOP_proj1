70241b26b6e5192e3c8149b69274aff07e84bee4
llpd1 <163384478+llpd1@users.noreply.github.com>
2025-10-02 17:38:12 +0900

    Update Student.hpp

--- Student.hpp
+++ Student.hpp
@@ -8,7 +8,8 @@ struct Student {
     std::string studentID;  // 정확히 10자리, 앞 4자리 = 입학년도
     int         birthYear{}; 
     std::string department; // 공백 포함 가능
-    std::string tel;        // 최대 12자리 //112213
+    std::string tel;        // 최대 12자리
+//***
 
     // 입학년도(학번 앞 4자리) 파생
     int admissionYear() const {
@@ -18,4 +19,5 @@ struct Student {
 };
 
 #endif // STUDENT_HPP
+
 
