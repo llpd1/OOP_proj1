123cff755af582f02019e5cd29a6ad91267ab3ca
jwon04lee <163104798+jwon04lee@users.noreply.github.com>
2025-10-11 23:27:29 +0900

    sortkey_ 디폴트값 설정

--- StudentDB.hpp
+++ StudentDB.hpp
@@ -58,7 +58,7 @@ private:
     // Internal state
     std::string                 path_;
     std::vector<Student>        data_;
-    SortKey                     sortKey_;
+    SortKey                     sortKey_{Sortkey::Name};
 
     // Separated components
     StudentValidator            validator_;           // Stateless: safe to hold as an instance
